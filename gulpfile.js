d7e27f050a2266c5bd3c17df274236c9695726fc
Bolaji Alade <Bolaji Alade>
2017-01-25 14:55:33 +0000

    add gulp Del module to clean prod before build & add gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
@@ -8,8 +7,9 @@ var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 var rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
+var del = require('del');
 
-// Static server
+// Run Static server & watch for file changes
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -17,30 +17,32 @@ gulp.task('browser-sync', function() {
         }
     });
 });
-
+//copy assets from node_modules folder to dev
 gulp.task('copy-node-assets', function () {
   return gulp.src(['./node_modules/materialize-css/sass/**/*.scss','!./node_modules/materialize-css/sass/ghpages*','!./node_modules/materialize-css/sass/style.scss'])
     .pipe(gulp.dest('./dev/sass'));
 });
-
+//copy fonts from dev to prod
 gulp.task('copy-node-fonts', function () {
   return gulp.src('./dev/fonts/**/*')
     .pipe(gulp.dest('./prod/fonts'));
 });
-
-
+//sass task
 gulp.task('sass', function () {
   return gulp.src('./dev/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.init())
     .pipe(concat('iai.css'))
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./prod/css'))
     .pipe(rename('iai.min.css'))
     .pipe(cssMin())
-    .pipe(gulp.dest('./prod/css'));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./prod/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
-
+//js task
 gulp.task('js', function () {
   return gulp.src('./dev/js/**/*.js')
     .pipe(sourcemaps.init())
@@ -49,19 +51,31 @@ gulp.task('js', function () {
     .pipe(gulp.dest('./prod/js'))
     .pipe(rename('iai.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./prod/js'));
+    .pipe(gulp.dest('./prod/js'))
+    .pipe(browserSync.reload({stream: true}))
 });
-
+//images task
 gulp.task('images', () =>
-    gulp.src('./dev/img/*')
+    gulp.src('./dev/img/*.+(jpg|jpeg|png|gif)')
         .pipe(imagemin())
         .pipe(gulp.dest('./prod/img/'))
+        .pipe(browserSync.reload({stream: true}))
 );
-
+//clean production folder before running build
+gulp.task('clean:prod', function () {
+  return del.sync([
+    'prod/**/*'
+    // '!prod/fonts/**/*'
+  ]);
+});
 // start server and watch files for changes
-gulp.task('watch', [ 'sass','browser-sync' ], function() {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', ['clean:prod','sass','js','images','copy-node-fonts','browser-sync'], function() {
+    gulp.watch('./dev/sass/*.scss', ['sass']);
+    gulp.watch('./dev/js/**/*.js', ['js']);
+    gulp.watch('./dev/img/**/*.+(jpg|png|gif)', ['images']);
     gulp.watch('./*.html').on('change', function() {
         browserSync.reload();
     });
 });
+//default task when 'gulp' is called without parameters
+gulp.task('default',['watch']);
